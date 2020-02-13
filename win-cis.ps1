edddc4a2c70ffe49d349c1b7a956c6eaf7f0f0db
Jerry Mozes <jerry@puppet.com>
2020-02-13 10:31:15 -0600

    Test Baseline DSC config

--- win-cis.ps1
+++ win-cis.ps1
@@ -1,2 +1,12 @@
 #Example CIS Hardening file
 
+Configuration Baseline
+{
+    Registry InteractiveLogon
+    {
+        Ensure      = "Present"  # You can also set Ensure to "Absent"
+        Key         = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+        ValueName   = "TestKey"
+        ValueData   = "0"
+    }
+}
