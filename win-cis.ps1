3ef379add9315ebf03daad633b289629735bb39a
Jerry Mozes <jerry@puppet.com>
2020-02-13 11:01:35 -0600

    Added comments

--- win-cis.ps1
+++ win-cis.ps1
@@ -1,5 +1,6 @@
 #Example CIS Hardening file
 
+#Defines Baseline configuration for a system
 Configuration Baseline
 {
     Registry InteractiveLogon
@@ -10,5 +11,9 @@ Configuration Baseline
         ValueData   = "0"
     }
 }
+
+#Compiles the Baseline Configuration to produce the MOF (Managed Object Format) file
 Baseline
+
+#Run the configuration on the machine
 Start-DSCConfiguration -Path Baseline -Verbose -Wait
