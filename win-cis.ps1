f5f7c4c013871860b44551e86a935cb2803f4714
Jerry Mozes <jerry@puppet.com>
2020-02-13 10:58:03 -0600

    Added to script Baseline and start config

--- win-cis.ps1
+++ win-cis.ps1
@@ -4,9 +4,11 @@ Configuration Baseline
 {
     Registry InteractiveLogon
     {
-        Ensure      = "Present"  # You can also set Ensure to "Absent"
+        Ensure      = "Present" 
         Key         = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
         ValueName   = "TestKey"
         ValueData   = "0"
     }
 }
+Baseline
+Start-DSCConfiguration -Path Baseline -Verbose -Wait
